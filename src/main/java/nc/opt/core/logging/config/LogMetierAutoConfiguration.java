64ce5ecb4c270b506238ba3add8e7504d5ba427e
ARNAL Jérôme <jerome.arnal@opt.nc>
2018-03-15 15:30:59 +1100

    feat(configure): add LogMetierService autoconfiguration

---
+++ src/main/java/nc/opt/core/logging/config/LogMetierAutoConfiguration.java
@@ -0,0 +1,14 @@
+package nc.opt.core.logging.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by 2816ARN on 15/03/2018.
+ */
+@Configuration
+@ComponentScan("nc.opt.core.logging")
+public class LogMetierAutoConfiguration {
+}
