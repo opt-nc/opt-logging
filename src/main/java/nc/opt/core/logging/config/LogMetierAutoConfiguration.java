11485a48cc009ad4c2215a32b632f79fa3aff12b
ARNAL Jérôme <jerome.arnal@opt.nc>
2018-03-15 15:40:32 +1100

    fix(sonar): fix sonar issues

--- src/main/java/nc/opt/core/logging/config/LogMetierAutoConfiguration.java
+++ src/main/java/nc/opt/core/logging/config/LogMetierAutoConfiguration.java
@@ -1,9 +1,7 @@
 package nc.opt.core.logging.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by 2816ARN on 15/03/2018.
